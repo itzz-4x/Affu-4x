906366aded5955047c9f05d4882bed4db9bcc77b
itzz-4x <mohammedanas4x@gmail.com>
2025-10-20 21:32:53 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// paste your config exactly as provided
+const firebaseConfig = {
+  apiKey: "AIzaSyB64KBoank1xHvTkzz4hDPG4Iz8YdGx-Vc",
+  authDomain: "princess-4x.firebaseapp.com",
+  projectId: "princess-4x",
+  storageBucket: "princess-4x.firebasestorage.app",
+  messagingSenderId: "796593115129",
+  appId: "1:796593115129:web:41ee6606da34b871553728",
+  measurementId: "G-2Y3VGK1CN5"
+};
+
+// Initialize
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
