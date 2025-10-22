13c98340906ec68b8d015693bc092f5e52c585fe
itzz-4x <mohammedanas4x@gmail.com>
2025-10-22 11:35:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
-// ✅ Initialize Firebase
-const app = firebase.initializeApp({
+// firebase-config.js
+const firebaseConfig = {
   apiKey: "AIzaSyB64KBoank1xHvTkzz4hDPG4Iz8YdGx-Vc",
   authDomain: "princess-4x.firebaseapp.com",
   projectId: "princess-4x",
-  storageBucket: "princess-4x.firebasestorage.app",
+  storageBucket: "princess-4x.appspot.com",
   messagingSenderId: "796593115129",
   appId: "1:796593115129:web:41ee6606da34b871553728",
   measurementId: "G-2Y3VGK1CN5"
-});
+};
 
-// ✅ Make Firestore & Auth available globally
+// ✅ Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
